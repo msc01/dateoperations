a4978a622a5cae40a468bb8da32617dd53d66eda
Michael Schwarze <michael@schwarze-web.de>
2016-06-16 20:58:44 +0200

    Updated rdoc.

--- README.md
+++ README.md
@@ -13,10 +13,16 @@ Download the gem, then install it:
 	require 'dateoperations'
 	...
 	DateOperations.country #=> :de
+	
 	DateOperations.holiday?(Date.new(2016, 10, 3)) #=> true
+	
 	DateOperations.country = :us #=> :us
 	DateOperations.holiday?(Date.new(2016, 10, 3)) # => false
+	
 	DateOperations.country = :de #=> :de
 	DateOperations.number_of_business_days_between(Date.new(2016, 10, 1), Date.new(2016, 10, -1)) #=> 20
+	
 	DateOperations.country = :au #=> :au
 	DateOperations.number_of_business_days_between(Date.new(2016, 10, 1), Date.new(2016, 10, -1)) #=> 21
+	
+	DateOperations.weekend?(DateOperations.weekend?(Date.new(2016, 6, 18)) #=> true
--- lib/dateoperations.rb
+++ lib/dateoperations.rb
@@ -1,38 +1,53 @@
 require 'date'
 require 'holidays'
 
-# Class: DateOperations -- Different date operations, primarily around business days
+# = DateOperations
+# Different date operations, primarily around business days, based on the Holidays[https://github.com/holidays/holidays] Gem.
+#
+# == Class: DateOperations
+# All date operations are implemented as class-level methods.
 class DateOperations
+
   @country = :de
 
   class << self
+    # Switches to the calendar of the respective country:
+    # - :de = Germany
+    # - :us = USA
+    # - ...
     attr_accessor :country
   end
 
+  # Returns an array with dates for the "week days" (Monday - Friday) between a given start and end date.
   def self.week_days_between(start_date, end_date)
     week_day_nbrs = (1..5)
     (start_date..end_date).select { |w| week_day_nbrs.include?(w.wday) }
   end
 
+  # returns the number of "week days" (Monday - Friday) between a given start and end date.
   def self.number_of_week_days_between(start_date, end_date)
     week_days = week_days_between(start_date, end_date)
     week_days.length
   end
 
+  # Returns an array with dates for the "business days" = "week days (Monday - Friday) without holidays" between a given start and end date.
   def self.business_days_between(start_date, end_date)
     week_days = week_days_between(start_date, end_date)
     week_days.reject { |b| holiday?(b) }
   end
 
+  # Returns the number of "business days" = "week days (Monday - Friday) without holidays" between a given start and end date.
   def self.number_of_business_days_between(start_date, end_date)
     business_days = business_days_between(start_date, end_date)
     business_days.length
   end
 
+  # Checks whether a given date is a weekend (Saturday, Sunday = true) or not (= false).
   def self.weekend?(date)
     date.saturday? || date.sunday?
   end
 
+  # Checks whether a given date is a holiday based on the calendar for the respective <tt>country</tt>. 
   def self.holiday?(date)
     return false if Holidays.on(date, DateOperations.country, :informal).empty?
     true
